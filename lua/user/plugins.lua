63c278a848c6bb8124b0022f7e7446dc67e8ad25
Ivan Aquino <ivan@codigoaxaca.com>
2022-05-15 12:12:02 -0500

    Added some colorschemes

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -62,6 +62,14 @@ return packer.startup(function(use)
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
+  use({"catppuccin/nvim", as = "catppuccin"})
+  use "wuelnerdotexe/vim-enfocado"
+  use "projekt0n/github-nvim-theme"
+  use "ellisonleao/gruvbox.nvim"
+  use "EdenEast/nightfox.nvim"
+  use "navarasu/onedark.nvim"
+  use({"rose-pine/neovim", as = "rose-pine"})
+  use "folke/tokyonight.nvim"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
